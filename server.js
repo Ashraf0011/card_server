a7f94c9c44ea55cd6089aa49b9f267d8bca28e75
Ash0011 <md_ashraful@icloud.com>
2022-12-01 20:54:32 +0930

    correction for index

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
 const crudRoutes = require('./CRUDfunc');
-var cors = require('cors');
+require('dotenv').config();
+// var cors = require('cors');
 
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log("remote database connected");
