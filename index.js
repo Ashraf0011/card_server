33e3ab1d8ea48e3bd56d61453ead502422f577e4
Ash0011 <md_ashraful@icloud.com>
2022-12-02 04:05:04 +0930

    .toml deleted, axios config changed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(
     cors(
         {
-            origin: ["https://visionary-gecko-559fb4.netlify.app/"],
+            origin: ["https://visionary-gecko-559fb4.netlify.app"],
             methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
             preflightContinue: true,
             credentials: true
