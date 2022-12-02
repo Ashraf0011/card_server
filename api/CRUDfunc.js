f68f64d028d4fcef6a56e03be6052499fb0a2f6a
Ash0011 <md_ashraful@icloud.com>
2022-12-03 03:23:46 +0930

    cord to url,res.header to all

--- api/CRUDfunc.js
+++ api/CRUDfunc.js
@@ -5,7 +5,7 @@ import CURD_SCHEMA from './schema';
 
 // Properly working version for vercel based backend.
 router.route('/').get((req, res) => {
-    res.header("Access-Control-Allow-Origin", true);
+    res.header("Access-Control-Allow-Origin", "*");
     res.send("Backend Connected!!");
 })
 
