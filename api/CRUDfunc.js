cb1e23d5b809b6afda3d7b6813a3450b423c155a
Ash0011 <md_ashraful@icloud.com>
2022-12-03 04:24:46 +0930

    probably working g

--- api/CRUDfunc.js
+++ api/CRUDfunc.js
@@ -13,7 +13,7 @@ router.route('/api/create').post((req, res) => {
     // 1. schema.save or schema.create both saves data to mongoDB
     // 2. we need schema to verify and validate data structure
     // 3. req.body is the recived object.  req.body is cardDetails itself.
-
+    res.header("Access-Control-Allow-Origin", "*");
     let CardDetails = req.body
     console.log("new req", CardDetails);
 
