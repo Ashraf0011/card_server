65e9366f438e7361b224ca83de07a59b2dc45b07
Ash0011 <md_ashraful@icloud.com>
2022-12-02 23:43:01 +0930

    **cors with info, **environment variables in vercel, **Access-Control-Allow-Origin, https://visionary-gecko-559fb4.netlify.app in CRUDfunction  **client side is does not need netlify.tomel

--- index.js
+++ index.js
@@ -3,23 +3,22 @@ import mongoose from 'mongoose';
 import crudRoutes from './api/CRUDfunc';
 require('dotenv').config();
 import cors from 'cors';
-const MONGO_URL = "mongodb+srv://DarkChocolate:ChocolateDark@dbcluster.nlm3zmb.mongodb.net/PaymentDetails?retryWrites=true&w=majority"
 
 const app = express();
 app.use(json());
 
 app.use(
     cors(
-        // {
-        //     origin: ["https://visionary-gecko-559fb4.netlify.app"],
-        //     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-        //     preflightContinue: true,
-        //     credentials: true
-        // }
+        {
+            origin: ["https://visionary-gecko-559fb4.netlify.app"],
+            methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+            preflightContinue: true,
+            credentials: true
+        }
     )
 );
 
-mongoose.connect(MONGO_URL, () => {
+mongoose.connect(process.env.MONGO_URL, () => {
     console.log("remote database connected");
 }).catch((e) => { console.log("Error Message:", e); })
 
