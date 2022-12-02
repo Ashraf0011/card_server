4fdda9d0afd2da49cbd6edff62d7619f011ddbf1
Ash0011 <md_ashraful@icloud.com>
2022-12-03 04:06:07 +0930

    probably working e

--- index.js
+++ index.js
@@ -7,16 +7,21 @@ import cors from 'cors';
 const app = express();
 app.use(json());
 
-app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-}));
+app.use(
+    cors({
+        origin: ["https://visionary-gecko-559fb4.netlify.app/"],
+        methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+        preflightContinue: true,
+        credentials: true
+    })
+);
 
 
-mongoose.connect(process.env.MONGO_URL, () => {
-    console.log("remote database connected");
-}).catch((e) => { console.log("Error Message:", e); })
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("remote database connected");
+    })
+    .catch((e) => { console.log("Error Message:", e); })
 
 
 // if we get '/' in call we will go to crud
