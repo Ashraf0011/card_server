e1bb3020ee6648d5a33df9b5a1d99a4a19325748
Ash0011 <md_ashraful@icloud.com>
2022-12-02 11:00:27 +0930

    cahnge server js to index in packeg json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const crudRoutes = require('./CRUDfunc');
+const crudRoutes = require('./api/CRUDfunc');
 require('dotenv').config();
 var cors = require('cors');
 
