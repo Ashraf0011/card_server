4852085b0a965ac67fc7551544c2729c1a8b3986
Ash0011 <md_ashraful@icloud.com>
2022-12-03 13:23:45 +0930

    probably worki on

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ import cors from 'cors';
 const app = express();
 app.use(json());
 
-app.use('/', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin, "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-
 app.use(
     cors({
         origin: "https://visionary-gecko-559fb4.netlify.app/",
